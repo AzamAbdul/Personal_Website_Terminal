bb8ef4a3add73d213ac6de5619247028dfb6196c
azmabdul <abdulkadirazam@gmail.com>
2016-06-09 16:23:19 -0400

    made the website much more responsive. began working on command support

--- javascript/indexjavascript.js
+++ javascript/indexjavascript.js
@@ -1,30 +1,26 @@
 var showText = function (target, message, curr_index, interval,cb) {   
-	if(curr_index == 0){
-		$(target).append("<p>")
-	}
+
 	if (curr_index < message.length) {
 
 		$(target).append(message[curr_index]);
-		alert($(target))
 		curr_index= curr_index+1
 		setTimeout(function () { showText(target, message, curr_index, interval,cb); }, interval);
 	}else{
 		console.log(message)
-		$(target).append("\n</p>");
+	
 
 		cb();
+		
 	}
 }
 var check_enter= function(){
-	$(document).keydown(function(evt) {
-
-		if (evt.keyCode === 13) {
 			$("authentication").hide();
 			$('.term').show();
-			blink(".cursor",true)
+			$('.term').addClass('animated slideInUp')
+			
 
-		}
-	});
+		
+	
 
 }
 var blink = function(element,check){
@@ -41,23 +37,42 @@ var blink = function(element,check){
 var driver = function(){
 
 	
-		showText("#authentication", " Hi, you've reached my website!", 0, 30,
+		showText("#authentication", " Hi, you've reached my website! ", 0, 30,
 			function() {setTimeout(function (){
 				showText("#authentication", "But. Wait. ", 0, 30, 
 					function(){setTimeout(function(){
-						showText("#authentication", "Something is missing?!? Can you help me rebuild it?", 0, 30, 
+						showText("#authentication", " Something is missing?!? Can you help me rebuild it?", 0, 30, 
 							function(){
-								showText("#authentication", "Take this! It might help ", 0, 30, function (){});
+								showText("#authentication", " Take this! It might help ", 0, 30, check_enter());
 							
 							});
 				},3000);});
 		},1500);});
 
 
-check_enter()
+
 
 
 }
-
+var is_valid_cmd = function(command){
+	return command ==="help" || command ==="ls"
+}
+var run_cmd = function(command){
+	if(command ==="help")
+		return "Supported commands include help,ls,echo "
+}
+$(document).ready(function(){
+    $('#command').keypress(function(e){
+      if(e.keyCode==13){
+      		var cmd_value = $('#command').val();
+      		if(is_valid_cmd(cmd_value)){
+				var result = run_cmd(cmd_value)       			
+				alert(result)
+      		}else{
+      			alert(cmd_value +" is not a valid command")
+      		}
+      }
+    });
+});
 $(document).ready(driver())
 
